11a3cdf67986ee72088a156fc48bcedf3456b8e2
Asandile Nkala <75987149+asandilenkala@users.noreply.github.com>
2024-08-09 20:47:39 +0200

    Update settings.py
    Updated STATICFILES_DIRS and added STATIC_ROOT

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -118,10 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_DIRS = [
-    BASE_DIR / "portfolio/static",
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_ROOT = [os.path.join(BASE_DIR, 'staticfiles', 'static')]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "portfolio/static",
+#]
 
 
 # Default primary key field type
