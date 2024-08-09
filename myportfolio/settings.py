8b20636f33d3a12dc129c6ef8c54dc062bf1b660
Asandile Nkala <75987149+asandilenkala@users.noreply.github.com>
2024-08-09 20:59:22 +0200

    Update settings.py
    Changing database from sqlite3 to postgresql

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -75,11 +75,22 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'uChVQyiBOeZOTSlLql1f',
+        'HOST': 'containers-us-west-196.railway.app',
+        'PORT': '6625',
+        
 }
 
 
