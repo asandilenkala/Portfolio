32d128523176b208c76593e35e77af8a45489a92
Asandile Nkala <75987149+asandilenkala@users.noreply.github.com>
2024-08-09 21:43:02 +0200

    Update settings.py
    removing new edits

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -75,21 +75,10 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'uChVQyiBOeZOTSlLql1f',
-        'HOST': 'containers-us-west-196.railway.app',
-        'PORT': '6625',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -129,11 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_ROOT = [os.path.join(BASE_DIR, 'staticfiles', 'static')]
-#STATICFILES_DIRS = [
-#    BASE_DIR / "portfolio/static",
-#]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    BASE_DIR / "portfolio/static",
+]
 
 
 # Default primary key field type
