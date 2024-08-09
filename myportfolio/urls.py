a9381df916e9d543bf3a810b1cbcc2b5012ccc43
Asandile Nkala <75987149+asandilenkala@users.noreply.github.com>
2024-08-09 20:51:55 +0200

    Update urls.py
    Added urlpatterns for MEDIA_ROOT & STATIC_ROOT

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -11,3 +11,6 @@ urlpatterns = [
     path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),  # Optional: Password reset.
     path('', include('portfolio.urls')),  # App URLs.
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
