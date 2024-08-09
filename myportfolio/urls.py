4b98aabf6d8a4b59ba58ce602a095b2b4ac7e417
Asandile Nkala <75987149+asandilenkala@users.noreply.github.com>
2024-08-09 21:41:59 +0200

    Update urls.py
    removing all new changes

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -12,5 +12,3 @@ urlpatterns = [
     path('', include('portfolio.urls')),  # App URLs.
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
