a9bbd7f2980e1b3500c73559e864a0f12fd61ab6
asandilenkala <asandilenkala@gmail.com>
2024-08-18 16:53:17 +0200

    Developing signup page.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User, Group
 from .models import Project
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
+from django import forms
 
 # Create your views here.
+
+# Custom Signup Form with Email
+class UserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+            user_group, created = Group.objects.get_or_create(name='Users')
+            user.groups.add(user_group)
+        return user
 
 def home(request):
     return render(request, 'portfolio/home.html')
@@ -16,6 +36,18 @@ def projects(request):
 
 def about(request):
     return render(request, 'portfolio/about.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            #return render(request, 'portfolio/login.html')
+            return redirect('home')  # Redirect to home after signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'portfolio/signup.html', {'form': form})
 
 def user_login(request):
     if request.method == 'POST':
